6dcfeb00a821e4ef16820846bfcbabc6f47d420e
Daniel Levett <dlevett@hotmail.co.uk>
2017-06-25 15:40:17 +0100

    Defined magic numbers as constants

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // @flow
 
+const DEFUALT_TUNING = 440;
+const NOTE_OFFSET = 69;
+const NOTES_PER_OCTAVE = 12;
+
 export const createLogBaseFn = (base: number) => (n: number): number => {
 	return Math.log(base) / Math.log(n);
 };
@@ -8,10 +12,10 @@ export const log2 = createLogBaseFn(2);
 
 export const log10 = createLogBaseFn(10);
 
-export const mtof = (note: number, tuning: number = 440): number => {
-	return Math.pow(2, (note - 69) / 12) * tuning;
+export const mtof = (note: number, tuning: number = DEFUALT_TUNING): number => {
+	return Math.pow(2, (note - NOTE_OFFSET) / NOTES_PER_OCTAVE) * tuning;
 };
 
-export const ftom = (frequency: number): number => {
-	return (12 * log2(frequency / 440)) + 69;
+export const ftom = (frequency: number, tuning: number = DEFUALT_TUNING): number => {
+	return (NOTES_PER_OCTAVE * log2(frequency / tuning)) + NOTE_OFFSET;
 };

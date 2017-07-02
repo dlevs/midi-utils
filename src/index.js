db7327f5fefdaf63da2c841d9a77fa6572ba6f5d
Daniel Levett <dlevett@hotmail.co.uk>
2017-07-02 20:57:01 +0100

    Use default prettier formatting

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const NOTE_OFFSET = 69;
 const NOTES_PER_OCTAVE = 12;
 
 export const createLogBaseFn = (base: number) => (n: number): number => {
-	return Math.log(base) / Math.log(n);
+  return Math.log(base) / Math.log(n);
 };
 
 export const log2 = createLogBaseFn(2);
@@ -13,15 +13,15 @@ export const log2 = createLogBaseFn(2);
 export const log10 = createLogBaseFn(10);
 
 export const midiToFrequency = (
-	note: number,
-	tuning: number = DEFUALT_TUNING
+  note: number,
+  tuning: number = DEFUALT_TUNING
 ): number => {
-	return Math.pow(2, (note - NOTE_OFFSET) / NOTES_PER_OCTAVE) * tuning;
+  return Math.pow(2, (note - NOTE_OFFSET) / NOTES_PER_OCTAVE) * tuning;
 };
 
 export const frequencyToMidi = (
-	frequency: number,
-	tuning: number = DEFUALT_TUNING
+  frequency: number,
+  tuning: number = DEFUALT_TUNING
 ): number => {
-	return NOTES_PER_OCTAVE * log2(frequency / tuning) + NOTE_OFFSET;
+  return NOTES_PER_OCTAVE * log2(frequency / tuning) + NOTE_OFFSET;
 };

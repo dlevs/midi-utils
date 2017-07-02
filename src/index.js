a37e9b637edc8d98ce8a12144142fbcbd55b2d9b
Daniel Levett <dlevett@hotmail.co.uk>
2017-07-02 17:52:29 +0100

    More descriptive function names

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ export const log2 = createLogBaseFn(2);
 
 export const log10 = createLogBaseFn(10);
 
-export const mtof = (note: number, tuning: number = DEFUALT_TUNING): number => {
+export const midiToFrequency = (note: number, tuning: number = DEFUALT_TUNING): number => {
 	return Math.pow(2, (note - NOTE_OFFSET) / NOTES_PER_OCTAVE) * tuning;
 };
 
-export const ftom = (frequency: number, tuning: number = DEFUALT_TUNING): number => {
+export const frequencyToMidi = (frequency: number, tuning: number = DEFUALT_TUNING): number => {
 	return (NOTES_PER_OCTAVE * log2(frequency / tuning)) + NOTE_OFFSET;
 };
